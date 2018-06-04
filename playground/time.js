781b40f6e91b8a7692e9415bf6eb9d35f8ecf25b
BuildTools <unconfigured@null.spigotmc.org>
2018-06-04 18:03:15 +0200

    Formatted timestamps using moment.js

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+// Jan 1st 1970 00:00:00 am UTC
+
+const moment = require('moment');
+
+// var date = moment();
+// date.add(1, 'y').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+var date = moment();
+console.log(date.format('k:mm'));
